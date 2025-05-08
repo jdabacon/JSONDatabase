d118beac8cd496ac435fb894f146d740cbfd41fe
jdabacon <125586119+jdabacon@users.noreply.github.com>
2025-05-08 23:18:05 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 10000 ;
+const port = process.env.PORT || 8080 ;
 
 app.use(express.json());
 app.use(express.static('public')); // For serving frontend files
